d2beae0bcb5fa8939b14e62e4ce7e9dde1a75805
Maycon <maycon.rvp@outlook.com>
2022-01-12 00:11:13 -0300

    projeto auth-vuex

---
+++ auth-vue-vuex/src/components/BarraNavegacaoDeslogado.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul class="navbar-nav mr-auto">
+    <li class="nav-item">
+      <router-link class="nav-link" to="/login">Login</router-link>
+    </li>
+    <li class="nav-item">
+      <router-link to="/cadastre-se" class="nav-link"> Registrar </router-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ auth-vue-vuex/src/views/NovoUsuario.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="container">
+    <h1>Novo usuário</h1>
+    <form @submit.prevent="enviarForm">
+      <div class="form-group">
+        <label for="nome">Nome</label>
+        <input
+          name="nome"
+          type="text"
+          class="form-control"
+          v-model="usuario.nome"
+        />
+      </div>
+
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input
+          name="email"
+          type="email"
+          class="form-control"
+          v-model="usuario.email"
+        />
+      </div>
+
+      <div class="form-group">
+        <label for="senha">Senha</label>
+        <input
+          name="senha"
+          type="password"
+          class="form-control"
+          v-model="usuario.senha"
+        />
+      </div>
+      <button class="btn btn-primary" type="submit">Salvar</button>
+    </form>
+  </div>
+</template>
+
+<script>
+// import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      usuario: {
+        nome: "",
+        email: "",
+        senha: "",
+      },
+    };
+  },
+  methods: {
+    enviarForm() {
+      this.$http
+        .post("auth/register", this.usuario)
+        .then((resposta) => {
+          console.log(resposta);
+          this.$router.push({ name: "login" });
+        })
+        .catch((erro) => console.log(erro));
+    },
+  },
+};
+</script>
+
+<style></style>
